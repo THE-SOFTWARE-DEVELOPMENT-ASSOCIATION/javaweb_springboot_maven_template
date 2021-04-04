ab8019795a67d2cca076b3f88472ec7a7339098c
qiweiran <apple@AppledeMacBook-Pro.local>
2021-04-04 23:41:08 +0800

    deleted unessasery files

---
+++ src/test/java/com/temp/BackendControlPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendControlPanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
